29e089a3fa636b982fb21be97d24b4618abf96be
Gustavo <gustavopolonio1@gmail.com>
2024-10-30 20:42:16 -0300

    feat: SEO

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { HelmetProvider } from 'react-helmet-async';
 import { Footer } from './components/Footer';
 import { Header } from './components/Header';
 import { Hero } from './components/Hero';
@@ -6,10 +7,19 @@ import { Section1 } from './components/Section1';
 import { Section2 } from './components/Section2';
 import { Section3 } from './components/Section3';
 import { Section4 } from './components/Section4';
+import { SEO } from './components/SEO';
 
 export function App() {
   return (
-    <>
+    <HelmetProvider>
+      <SEO
+        title="Soller | Power your home"
+        description="Soller offers complete solar energy solutions for homes, providing savings and sustainability. Discover how to harness the power of the sun and reduce your energy bills with our specialized service."
+        keywords="Soller, solar panels, solar energy, home solar power, renewable energy, sustainable energy, solar solutions, energy savings"
+        name="Soller"
+        type="website"
+      />
+
       <Header />
       <Hero />
       <Section1 />
@@ -18,6 +28,6 @@ export function App() {
       <Reviews />
       <Section4 />
       <Footer />
-    </>
+    </HelmetProvider>
   );
 }
---
+++ src/components/SEO.tsx
@@ -0,0 +1,28 @@
+import { Helmet } from 'react-helmet-async';
+
+interface SEOProps {
+  title: string
+  description: string
+  name: string
+  type: string
+  keywords: string
+}
+
+export function SEO({
+  title, description, name, type, keywords,
+}: SEOProps) {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
+      <meta property="og:type" content={type} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta name="twitter:creator" content={name} />
+      <meta name="twitter:card" content={type} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+    </Helmet>
+  );
+}
