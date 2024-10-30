848e121513d0c6ec31f91c0c6f69837949b7b607
Gustavo <gustavopolonio1@gmail.com>
2024-10-30 18:16:45 -0300

    sections 1,2 and 3 responsiveness

--- src/components/Section1.tsx
+++ src/components/Section1.tsx
@@ -1,16 +1,16 @@
 export function Section1() {
   return (
-    <section className="pt-12 pb-[358px] text-center relative">
-      <span className="padded text-amber-600 font-medium">No more waste</span>
-      <h2 className="padded mt-2 font-bold text-[32px] leading-9">Pick the Sun</h2>
-      <p className="padded mt-6">
+    <section className="pt-12 pb-[358px] text-center relative sm:pb-12">
+      <span className="padded text-amber-600 font-medium lg:text-xl">No more waste</span>
+      <h2 className="padded mt-2 font-bold text-[32px] leading-9 lg:text-[56px] lg:leading-[60px] lg:font-extrabold">Pick the Sun</h2>
+      <p className="padded mt-6 sm:w-[80%] sm:mx-auto sm:max-w-[900px] lg:text-xl">
         Et pulvinar nec interdum integer id urna molestie porta nullam. A, donec ornare sed
         turpis pulvinar purus maecenas quam a. Erat porttitor pharetra sed in mauris
         elementum sollicitudin.
       </p>
-      <img className="absolute left-0" src="/images/mobile/feature1-left-circle.png" alt="" />
-      <img className="absolute right-0" src="/images/mobile/feature1-right-circle.png" alt="" />
-      <img className="absolute bottom-12 right-0 w-[358px] shadow-image" src="/images/mobile/feature1.png" alt="Dashboard design" />
+      <img className="absolute left-0 -z-10 sm:w-[24%]" src="/images/mobile/feature1-left-circle.png" alt="" />
+      <img className="absolute right-0 -z-10 sm:w-[10%]" src="/images/mobile/feature1-right-circle.png" alt="" />
+      <img className="absolute bottom-12 right-0 w-[358px] shadow-image sm:static sm:mx-auto sm:mt-16 sm:w-[80%] sm:max-w-screen-2xl" src="/images/mobile/feature1.png" alt="Dashboard design" />
     </section>
   );
 }
--- src/components/Section3.tsx
+++ src/components/Section3.tsx
@@ -1,44 +1,48 @@
 export function Section3() {
   return (
-    <section className="py-12 text-center relative">
-      <span className="padded text-amber-600 font-medium">Services</span>
-      <h2 className="padded mt-2 font-bold text-[32px] leading-9">Personalized services</h2>
-      <p className="padded mt-6">
-        Pretium lectus ultrices sit tempor, sit ullamcorper volutpat et et. Auctor turpis
-        semper id sit ornare maecenas lectus sed.
-      </p>
-      <ul className="padded mt-16 grid grid-cols-2 gap-12">
-        <li className="flex flex-col space-y-4">
-          <strong className="font-medium text-xl leading-[22px]">Et mauris</strong>
-          <span>
-            Posuere quis sed mauris non curabitur pretium elementum eget. Feugiat sed maecenas eu
-            accumsan tristique.
-          </span>
-        </li>
-        <li className="flex flex-col space-y-4">
-          <strong className="font-medium text-xl leading-[22px]">Eget sit</strong>
-          <span>
-            Sit bibendum donec dolor fames neque vulputate non sit aliquam. Consequat turpis
-            natoque leo, massa.
-          </span>
-        </li>
-        <li className="flex flex-col space-y-4">
-          <strong className="font-medium text-xl leading-[22px]">Imperdiet pellentesque</strong>
-          <span>
-            Platea arcu dapibus non magna cursus lectus id sollicitudin. Enim viverra parturient
-            tristique nulla.
-          </span>
-        </li>
-        <li className="flex flex-col space-y-4">
-          <strong className="font-medium text-xl leading-[22px]">Non libero</strong>
-          <span>
-            Congue mauris sem vel, urna viverra. Urna, nibh leo suscipit purus ut sed eros,
-            consectetur viverra.
-          </span>
-        </li>
-      </ul>
+    <section className="py-12 text-center relative md:flex md:items-center lg:text-start">
+      <div>
+        <span className="padded text-amber-600 font-medium lg:text-xl">System features</span>
+        <h2 className="padded mt-2 font-bold text-[32px] leading-9 lg:text-[56px] lg:leading-[60px] lg:font-extrabold">Powerful features</h2>
+        <p className="padded mt-6 lg:text-xl">
+          Pretium lectus ultrices sit tempor, sit ullamcorper volutpat et et. Auctor turpis
+          semper id sit ornare maecenas lectus sed.
+        </p>
+        <ul className="padded mt-16 grid grid-cols-2 gap-12">
+          <li className="flex flex-col space-y-4">
+            <strong className="font-medium text-xl leading-[22px]">Et mauris</strong>
+            <span>
+              Posuere quis sed mauris non curabitur pretium elementum eget. Feugiat sed maecenas eu
+              accumsan tristique.
+            </span>
+          </li>
+          <li className="flex flex-col space-y-4">
+            <strong className="font-medium text-xl leading-[22px]">Eget sit</strong>
+            <span>
+              Sit bibendum donec dolor fames neque vulputate non sit aliquam. Consequat turpis
+              natoque leo, massa.
+            </span>
+          </li>
+          <li className="flex flex-col space-y-4">
+            <strong className="font-medium text-xl leading-[22px]">Imperdiet pellentesque</strong>
+            <span>
+              Platea arcu dapibus non magna cursus lectus id sollicitudin. Enim viverra parturient
+              tristique nulla.
+            </span>
+          </li>
+          <li className="flex flex-col space-y-4">
+            <strong className="font-medium text-xl leading-[22px]">Non libero</strong>
+            <span>
+              Congue mauris sem vel, urna viverra. Urna, nibh leo suscipit purus ut sed eros,
+              consectetur viverra.
+            </span>
+          </li>
+        </ul>
+      </div>
       <img className="absolute right-0 -z-10" src="/images/mobile/feature3-rectangle.png" alt="" />
-      <img className="mt-16 mx-auto shadow-image" src="/images/mobile/feature3.png" alt="Smartphone design" />
+      <div className="w-[220px] mt-16 mx-auto md:mt-0 md:shrink-0 md:padded lg:w-[360px]">
+        <img className="w-full shadow-image md:static" src="/images/mobile/feature3.png" alt="Smartphone design" />
+      </div>
     </section>
   );
 }
